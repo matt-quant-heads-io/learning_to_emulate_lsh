5a3bc5aa76782e283962e4a7921821f5f0ebfeed
matt-quant-heads-io <matt.quantheads.io@gmail.com>
2022-12-21 21:46:54 -0500

    Updated run_setup.sh

--- run_setup.sh
+++ run_setup.sh
@@ -15,6 +15,19 @@ rm $CURR_DIR/datasets/glove.6B.zip
 
 echo "Downloaded GloVe datasets"
 
+echo "Creating models directories"
+
+for subdir in 10_clusters_10000_num_words 100_clusters_10000_num_words 10_clusters_20000_num_words 100_clusters_20000_num_words 10_clusters_50000_num_words 100_clusters_50000_num_words
+do
+  mkdir -p $CURR_DIR/models/inter_cluster_models/$subdir
+done
+
+echo "Creating experiments directories"
+for subdir in results
+do
+  mkdir -p $CURR_DIR/experiments/$subdir
+done
+
 
 
 
